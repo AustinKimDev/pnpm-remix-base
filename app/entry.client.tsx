76e431655beecb8a5bc118e7643254d198e036ba
Apitots <apitots@kakao.com>
2023-02-24 16:23:56 +0900

    index page 작성

---
+++ app/entry.client.tsx
@@ -0,0 +1,22 @@
+import { hydrateRoot } from "react-dom/client";
+import { startTransition, StrictMode } from "react";
+import { RemixBrowser } from "@remix-run/react";
+
+const hydrate = () => {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    );
+  });
+};
+
+if (typeof requestIdleCallback === "function") {
+  requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1);
+}
